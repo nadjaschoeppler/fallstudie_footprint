51e9a210a1902ad82feb86eacc8f8237948f9504
nadjaschoeppler <nadja.herbstfarben@posteo.de>
2023-09-10 11:56:04 +0200

    Filter für Tabelle geschrieben

--- js/footprints.js
+++ js/footprints.js
@@ -6,12 +6,12 @@ function sortTable(table, col, reverse) {
   reverse = -((+reverse) || -1);
   tr = tr.sort(function (a, b) { // sort rows
     return reverse // `-1 *` if want opposite order
-        * (a.cells[col].textContent.trim()
+        * (a.cells[col].textContent.trim() 
             .localeCompare(b.cells[col].textContent.trim())
            );
   });
-  for(var i = 0; i < tr.length; ++i)
-        tb.appendChild(tr[i]);
+  for(var i = 0; i < tr.length; ++i) 
+        tb.appendChild(tr[i]); 
 }
 /* simplify the sortTable function by directly accessing the tables */
 function sortByCountry() {
@@ -20,3 +20,22 @@ function sortByCountry() {
 function sortByEmission() {
     sortTable(document.getElementById('footprint-table'), 1);
   }
+
+  /* a function to filter a table by a column */
+function filterTable(table, col, query) {
+    var tb = table.tBodies[0]
+    var tr = Array.prototype.slice.call(tb.rows, 0);
+    query = query.toLowerCase();
+    for(var i = 0; i < tr.length; ++i) {
+        var td = tr[i].cells[col].textContent.toLowerCase();
+        tr[i].style.display = td.indexOf(query) > -1 ? '' : 'none';
+    }
+    }
+
+    /* simplify the filterTable function by directly accessing the tables */
+function filterByCountry() {
+    filterTable(document.getElementById('footprint-table'), 0, document.getElementById('country').value);
+}
+function filterByEmission() {
+    filterTable(document.getElementById('footprint-table'), 1, document.getElementById('emission').value);
+}
