e72f0287a15f2b098ebcd3fb4021fcae5c8dc134
nadjaschoeppler <nadja.herbstfarben@posteo.de>
2023-09-14 07:38:53 +0200

    Filtern verbessert

--- js/footprints.js
+++ js/footprints.js
@@ -1,24 +1,47 @@
+
+var sortOrderCountry = -1;
+var sortOrderEmission = -1;
+
 /* a function to sort a table by a column */
-function sortTable(table, col, reverse) {
+function sortTable(table, col, reverse, compareFunction) {
   var tb = table.tBodies[0]
   var tr = Array.prototype.slice.call(tb.rows, 0);
 
   reverse = -((+reverse) || -1);
-  tr = tr.sort(function (a, b) { // sort rows
-    return reverse // `-1 *` if want opposite order
-        * (a.cells[col].textContent.trim()
-            .localeCompare(b.cells[col].textContent.trim())
-           );
-  });
+  tr = tr.sort( (a,b) =>
+      compareFunction(a, b,col,reverse)
+);
+    console.log(tr)
   for(var i = 0; i < tr.length; ++i)
+  {
+        console.log(tr[i])
         tb.appendChild(tr[i]);
+  }
+}
+
+function compareTextcells(a, b,col,reverse) {
+
+    return reverse *
+    (
+        a.cells[col].textContent.trim()
+        .localeCompare(b.cells[col].textContent.trim())
+    )
+}
+function compareNumbercells(a, b,col,reverse) {
+
+    return reverse *
+    (
+        Number(a.cells[col].textContent.trim().replace(".","")) -Number(b.cells[col].textContent.trim().replace(".",""))
+    )
 }
 /* simplify the sortTable function by directly accessing the tables */
 function sortByCountry() {
-  sortTable(document.getElementById('footprint-table'), 0);
+  sortTable(document.getElementById('footprint-table'), 0,sortOrderCountry, compareTextcells);
+  sortOrderCountry *= -1;
 }
 function sortByEmission() {
-    sortTable(document.getElementById('footprint-table'), 1);
+    sortTable(document.getElementById('footprint-table'), 1,sortOrderEmission, compareNumbercells);
+    sortOrderEmission *= -1;
   }
 
   /* a function to filter a table by a column */
@@ -28,12 +51,13 @@ function filterTable(table, col, query) {
     query = query.toLowerCase();
     for(var i = 0; i < tr.length; ++i) {
         var td = tr[i].cells[col].textContent.toLowerCase();
-        tr[i].style.display = td.indexOf(query) > -1 ? '' : 'none';
+        tr[i].style.display = td.indexOf(query) ===0 ? '' : 'none';
     }
     }
 
     /* simplify the filterTable function by directly accessing the tables */
 function filterByCountry() {
+
     filterTable(document.getElementById('footprint-table'), 0, document.getElementById('country').value);
 }
 function filterByEmission() {
